9a7851f4cffb809bae247cde097e3f652b5ac220
BuildTools <78happydragon78@gmail.com>
2019-12-15 01:21:59 -0500

    Organization and naming

--- KNN.py
+++ KNN.py
@@ -1,18 +1,19 @@
+# K Nearest Neighbors Machine Learning using Tensor Libraries
+# ---Credit---
 # Marko Bohanec. UCI Machine Learning Repository [https://archive.ics.uci.edu/ml/datasets/car+evaluation].
 
 import sklearn
-from sklearn.utils import shuffle
 from sklearn.neighbors import KNeighborsClassifier
 import pandas as pd
-import numpy as np
-from sklearn import linear_model, preprocessing
+from sklearn import preprocessing
 
 carData = pd.read_csv("car.data", sep=",")
 
 # transforming values into numerical values
+# if there was an array like [low, medium, high] then the encoder would change it to [0, 1, 2]
 encoder = preprocessing.LabelEncoder()
 buying = encoder.fit_transform(list(carData["buying"]))
-maint = encoder.fit_transform(list(carData["maint"]))
+maint = encoder.fit_transform(list(carData["technical_charac"]))
 doors = encoder.fit_transform(list(carData["doors"]))
 persons = encoder.fit_transform(list(carData["persons"]))
 lug_boot = encoder.fit_transform(list(carData["lug_boot"]))
@@ -33,13 +34,17 @@ x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X, Y
 # how many closest points it should look for
 kValue = KNeighborsClassifier(n_neighbors=9)
 
+# slap the data into the line
 kValue.fit(x_train, y_train)
+# accuracy score of the data
 accuracy = kValue.score(x_test, y_test)
 print("Accuracy: ", "%.01f" % (accuracy * 100), "%", sep="")
 
+# predict for the info on the data collected
 predictedVal = kValue.predict(x_test)
-names = ["bad", "okay", "good", "very_good"]
+# replacement names for the 1-4 values
+rep_names = ["bad", "okay", "good", "very_good"]
 
 for x in range(len(predictedVal)):
-    print("Predicted: ", names[predictedVal[x]], " | Data: ", x_test[x], " | Actual: ", names[y_test[x]], sep="")
+    print("Predicted: ", rep_names[predictedVal[x]], " | Data: ", x_test[x], " | Actual: ", rep_names[y_test[x]], sep="")
     neighbDistance = kValue.kneighbors([x_test[x]], 9, True)
