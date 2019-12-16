448bda35596ceec5fc96e35d48c89a3950fd5e7e
BuildTools <78happydragon78@gmail.com>
2019-12-15 23:18:50 -0500

    Should be final

--- KNN.py
+++ KNN.py
@@ -1,4 +1,4 @@
-# K Nearest Neighbors Machine Learning using Tensor Libraries
+# K Nearest Neighbors Machine Learning using Tensorflow Libraries
 # ---Credit---
 # Marko Bohanec. UCI Machine Learning Repository [https://archive.ics.uci.edu/ml/datasets/car+evaluation].
 
@@ -7,6 +7,7 @@ from sklearn.neighbors import KNeighborsClassifier
 import pandas as pd
 from sklearn import preprocessing
 
+# read in our car.data file
 carData = pd.read_csv("car.data", sep=",")
 
 # transforming values into numerical values
@@ -20,9 +21,6 @@ lug_boot = encoder.fit_transform(list(carData["lug_boot"]))
 safety = encoder.fit_transform(list(carData["safety"]))
 classc = encoder.fit_transform(list(carData["class"]))
 
-# what computer is attempting to predict
-predictGoal = "class"
-
 # split data into a list of goals and a list of training data
 X = list(zip(buying, maint, doors, persons, lug_boot, safety))
 Y = list(classc)
@@ -32,19 +30,21 @@ Y = list(classc)
 x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X, Y, test_size=0.1)
 
 # how many closest points it should look for
-kValue = KNeighborsClassifier(n_neighbors=9)
+k_value = KNeighborsClassifier(n_neighbors=9)
 
 # slap the data into the line
-kValue.fit(x_train, y_train)
+k_value.fit(x_train, y_train)
 # accuracy score of the data
-accuracy = kValue.score(x_test, y_test)
+accuracy = k_value.score(x_test, y_test)
 print("Accuracy: ", "%.01f" % (accuracy * 100), "%", sep="")
 
 # predict for the info on the data collected
-predictedVal = kValue.predict(x_test)
+predicted_val = k_value.predict(x_test)
 # replacement names for the 1-4 values
-rep_names = ["bad", "okay", "good", "very_good"]
+rep_names = ["bad", "okay", "good", "very good"]
 
-for x in range(len(predictedVal)):
-    print("Predicted: ", rep_names[predictedVal[x]], " | Data: ", x_test[x], " | Actual: ", rep_names[y_test[x]], sep="")
-    neighbDistance = kValue.kneighbors([x_test[x]], 9, True)
+# run through all our testing data
+for x in range(len(predicted_val)):
+    print("Predicted: ", rep_names[predicted_val[x]], " | Data: ", x_test[x], " | Actual: ", rep_names[y_test[x]], sep="")
+    # not needed right now, but shows the distances of all the neighbors
+    neighbor_distance = k_value.kneighbors([x_test[x]], 9, True)
